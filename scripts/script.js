e61fde7d5f69c42fc091d6e7d596a0c2d3d93ea5
Hiago Henrique Gomes <hiago_h3@hotmail.com>
2022-05-15 18:42:38 -0300

    estilização h2 resultado

--- scripts/script.js
+++ scripts/script.js
@@ -1,15 +1,15 @@
 document.querySelector('input[type=number]').oninvalid = function() {
 
-    // remove mensagens de erro antigas
-    this.setCustomValidity("");
-  
-    // reexecuta validação
-    if (!this.validity.valid) {
-  
-        // se inválido, coloca mensagem de erro
-        this.setCustomValidity("Adicione somente numeros, sem caracteres especiais ou letras.");
-    } 
-  };
+  // remove mensagens de erro antigas
+  this.setCustomValidity("");
+
+  // reexecuta validação
+  if (!this.validity.valid) {
+
+      // se inválido, coloca mensagem de erro
+      this.setCustomValidity("Adicione somente numeros, sem caracteres especiais ou letras.");
+  } 
+};
 
 
   function gerar() {
