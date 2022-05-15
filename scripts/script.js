b36a5105bfc733125b08d2ede79b3a53addbb5e7
Hiago Henrique Gomes <hiago_h3@hotmail.com>
2022-05-15 18:24:19 -0300

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -1,21 +1,21 @@
-document.querySelector('input[type=number]').oninvalid = function() {
+// document.querySelector('input[type=number]').oninvalid = function() {
 
-  // remove mensagens de erro antigas
-  this.setCustomValidity("");
+//   // remove mensagens de erro antigas
+//   this.setCustomValidity("");
 
-  // reexecuta validação
-  if (!this.validity.valid) {
+//   // reexecuta validação
+//   if (!this.validity.valid) {
 
-      // se inválido, coloca mensagem de erro
-      this.setCustomValidity("Adicione somente numeros, sem caracteres especiais ou letras.");
-  } 
-};
+//       // se inválido, coloca mensagem de erro
+//       this.setCustomValidity("Adicione somente numeros, sem caracteres especiais ou letras.");
+//   } 
+// };
 
 
-  function gerar() {
-    var numero = document.querySelector('input#number')
-    var res = document.getElementById('res')
-    var n1 = Number(numero.value)
-    var wpp = n1
-    res.innerHTML = `Seu link do Whatsapp é: https://wa.me/${n1}`
-  }
+//   function gerar() {
+//     var numero = document.querySelector('input#number')
+//     var res = document.getElementById('res')
+//     var n1 = Number(numero.value)
+//     var wpp = n1
+//     res.innerHTML = `Seu link do Whatsapp é: https://wa.me/${n1}`
+//   }
